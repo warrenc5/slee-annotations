d20adc5f9195e8795a113a3fbbda525968d2872e
Warren Crossing <warren@itcl.com.au>
2023-07-13 20:01:45 +1000

    add test example

--- examples/src/main/java/org/mobicents/slee/annotations/examples/sbb/ExampleSbbLocalObject.java
+++ examples/src/main/java/org/mobicents/slee/annotations/examples/sbb/ExampleSbbLocalObject.java
@@ -25,7 +25,7 @@ package org.mobicents.slee.annotations.examples.sbb;
 import javax.slee.SbbLocalObject;
 
 
-public interface ExampleSbbLocalObject extends SbbLocalObject {
+public interface ExampleSbbLocalObject extends SbbLocalObject , SecondaryInterface{
 
 	public boolean doSomething();
 	public String getAnotherCMPField();
---
+++ processor/src/test/java/mobi/mofokom/javax/slee/annotations/examples/sbb/NotExtendsSbbLocalObject.java
@@ -0,0 +1,12 @@
+package mobi.mofokom.javax.slee.annotations.examples.sbb;
+
+import java.util.function.Consumer;
+import javax.slee.SbbLocalObject;
+
+/**
+ *
+ * @author wozza
+ */
+public interface NotExtendsSbbLocalObject extends SbbLocalObject, Consumer {
+
+}
