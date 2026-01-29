0d41bc543c39ddf84bb9f54fbe60b43833716996
Warren Crossing <warren@itcl.com.au>
2026-01-29 15:54:02 +1100

    release fixups

--- annotations/src/main/java/module-info.java
+++ annotations/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module mobi.mofokom.JainSleeAnnotations {
     exports mobi.mofokom.javax.slee.annotation;
     exports mobi.mofokom.javax.slee.annotation.event;
+    requires jainslee.api;
+    requires ejb.api;
 }
